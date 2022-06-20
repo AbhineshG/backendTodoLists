e8919336dda1bbf73664f103c4a9f93e6e3fce5f
abhinesh g <abhineshg04@gmail.com>
2022-06-20 13:11:14 +0530

    firsts commit

--- routes/todoesRoutes.js
+++ routes/todoesRoutes.js
@@ -9,3 +9,15 @@ router.delete("/:id",deleteTodos)
 
 
 export default router
+
+// 1. git init
+
+// 2. git add .
+
+// 3. git status
+
+// 4. git commit -m 'your message'
+
+// 5. git remote add origin 'your_url_name' 
+
+// 6. git push -u origin master //then login w/ your creds
